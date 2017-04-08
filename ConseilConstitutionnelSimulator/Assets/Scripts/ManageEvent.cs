3f74053070ddba7aad703def27d0c6c9ca636305
MorganeLeBris <morganebris@gmail.com>
2017-04-08 09:38:16 -0400

    Début Boutons

---
+++ ConseilConstitutionnelSimulator/Assets/Scripts/ManageEvent.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManageEvent : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ClickValider()
+    {
+        Debug.Log("V");
+    }
+
+    public void ClickAccentuer()
+    {
+        Debug.Log("A");
+    }
+
+    public void ClickDiminuer()
+    {
+        Debug.Log("D");
+    }
+
+    public void ClickRefuser()
+    {
+        Debug.Log("R");
+    }
+}
