5b43e3e2ce20dd2d5af76d88ae7ff247e41cc1db
Tiras69 <mathm69@gmail.com>
2017-04-08 16:48:11 +0200

    Law Model + Serialization xml

---
+++ ConseilConstitutionnelSimulator/Assets/Scripts/XmlSerializerHelper.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+using UnityEngine;
+
+public class XmlSerializerHelper<T> where T : class
+{
+    
+    public static void SerializeXmlFile(string _fileName, T _law)
+    {
+        XmlSerializer serializer = new XmlSerializer(typeof(T));
+        using(StreamWriter writer = new StreamWriter(_fileName))
+        {
+            serializer.Serialize(writer, _law);
+        }
+    }
+
+    public static T DeserializeXmlFile(string _fileName)
+    {
+        XmlSerializer serializer = new XmlSerializer(typeof(T));
+        using (StreamReader reader = new StreamReader(_fileName))
+        {
+            return serializer.Deserialize(reader) as T;
+        }
+
+        return default(T);
+    }
+
+}
---
+++ ConseilConstitutionnelSimulator/Assets/Scripts/testSerialize.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testSerialize : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        Deserialize();
+
+	}
+	
+    private void SerializeTest()
+    {
+        Law law = new Law();
+        law.Title = "coucou test title";
+        law.Description = "Description incroyable";
+        law.YesPropertyModifiers.Add(new PropertyModifier(GameProperty.GameProperty_ECONOMY, 10));
+        law.NoPropertyModifiers.Add(new PropertyModifier(GameProperty.GameProperty_GOVERNMENTOPINION, 10));
+        law.MaximizeLawsToAdd.Add(5);
+        law.MinimizeLawsToAdd.Add(1);
+
+        XmlSerializerHelper<Law>.SerializeXmlFile("Test.xml", law);
+    }
+
+    private void Deserialize()
+    {
+        Law law;
+        law = XmlSerializerHelper<Law>.DeserializeXmlFile("Test.xml");
+        Debug.Log("coucou");
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
