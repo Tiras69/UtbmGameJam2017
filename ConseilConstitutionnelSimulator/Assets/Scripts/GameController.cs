8bc81c43e1342c4a1eaf67a4f76d5002c913524f
pguimaraes1234567890 <dragon_killer_68@hotmail.fr>
2017-04-08 16:31:40 +0200

    text change on new Event

---
+++ ConseilConstitutionnelSimulator/Assets/Scripts/GameController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+
+    public Text title;
+    public Text law;
+
+    public List<string> titles;
+    public List<string> laws;
+
+    // Use this for initialization
+    void Start () {
+        this.newEvent();
+
+        this.titles.Add("test");
+        this.laws.Add("test");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+ 
+    }
+
+    void newEvent()
+    {
+        // inclusive , exclusive
+        int random = Random.Range(0, titles.Count);
+
+        this.title.text = titles[random];
+        this.law.text = laws[random];
+
+    }
+}
