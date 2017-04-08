048d9e54f5237a2d4fe72fe78d5dc4b4f38f686b
Tiras69 <mathm69@gmail.com>
2017-04-08 18:18:08 +0200

    Game Manager begin

---
+++ ConseilConstitutionnelSimulator/Assets/Scripts/GameManager.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState
+{
+    GameState_MENU,
+    GameState_BEGINSEMESTER,
+    GameState_DECIDELAW,
+    GameState_SEMESTERREPORT,
+    GameState_ENDSEMESTER
+}
+
+public class GameManager : Singleton<GameManager> {
+
+    #region constructors
+    protected GameManager()
+    {
+        m_currentGameState = GameState.GameState_MENU;
+        m_allGameLaws = new List<Law>(100);
+        m_currentGameSessionLaws = new List<Law>(100);
+        m_currentMonthInSemester = 0;
+    }
+    #endregion
+
+    /// <summary>
+    /// Those fields must never be modify outside this manager
+    /// all modification must be done in methods.
+    /// </summary>
+    #region fields
+    private GameState m_currentGameState;
+
+    private int m_governmentOpinion;
+    private int m_populaceOpinion;
+    private int m_personalMoney;
+    private int m_economy;
+    private int m_employement;
+    private int m_religion;
+
+    List<Law> m_allGameLaws;
+    List<Law> m_currentGameSessionLaws;
+    private int m_currentMonthInSemester;
+
+    private Random m_randomNumberGenerator = new Random();
+
+    #endregion
+
+    #region Methods
+    public void LoadAllLaws()
+    {
+
+    }
+
+    public Law GetNextLaw()
+    {
+        if (m_currentMonthInSemester < 6)
+        {
+            m_currentGameState = GameState.GameState_DECIDELAW;
+            int nextIndex = m_randomNumberGenerator.Next(0, m_currentGameSessionLaws.Count - 1);
+            m_currentMonthInSemester++;
+            return m_currentGameSessionLaws[nextIndex];
+        }
+        else
+        {
+            StartSemesterReport();
+            m_currentMonthInSemester = 0;
+        }
+        return null;
+    }
+
+    public void StartSemesterReport()
+    {
+        m_currentGameState = GameState.GameState_SEMESTERREPORT;
+    }
+
+    #endregion
+
+
+
+}
--- ConseilConstitutionnelSimulator/Assets/Scripts/testSerialize.cs
+++ ConseilConstitutionnelSimulator/Assets/Scripts/testSerialize.cs
@@ -7,21 +7,36 @@ public class testSerialize : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 
-        Deserialize();
-
+        SerializeTest();
 	}
 	
     private void SerializeTest()
     {
         Law law = new Law();
-        law.Title = "coucou test title";
-        law.Description = "Description incroyable";
-        law.YesPropertyModifiers.Add(new PropertyModifier(GameProperty.GameProperty_ECONOMY, 10));
-        law.NoPropertyModifiers.Add(new PropertyModifier(GameProperty.GameProperty_GOVERNMENTOPINION, 10));
-        law.MaximizeLawsToAdd.Add(5);
-        law.MinimizeLawsToAdd.Add(1);
+        law.Title = "Titre 1";
+        law.Description = "Description Lorem Ipsum";
 
-        XmlSerializerHelper<Law>.SerializeXmlFile("Test.xml", law);
+        law.YesPropertyModifiers.Add(new PropertyModifier(GameProperty.GameProperty_ECONOMY, 10));
+        law.YesPropertyModifiers.Add(new PropertyModifier(GameProperty.GameProperty_EMPLOYMENT, 9));
+        law.YesPropertyModifiers.Add(new PropertyModifier(GameProperty.GameProperty_GOVERNMENTOPINION, 53));
+
+        law.YesLawsToAdd.Add(3);
+        law.YesLawsToAdd.Add(8);
+
+        law.NoPropertyModifiers.Add(new PropertyModifier(GameProperty.GameProperty_PERSONALMONEY, 1000));
+        law.NoPropertyModifiers.Add(new PropertyModifier(GameProperty.GameProperty_POPULACEOPINION, -90));
+        law.NoPropertyModifiers.Add(new PropertyModifier(GameProperty.GameProperty_RELIGION, 8));
+
+        law.NoLawsToAdd.Add(5);
+        law.NoLawsToAdd.Add(8);
+
+        
+
+        
+        law.MinimizeLawsToAdd.Add(1);
+        law.MinimizePropertyModifiers.Add(new PropertyModifier(GameProperty.GameProperty_GOVERNMENTOPINION, -60));
+
+        XmlSerializerHelper<Law>.SerializeXmlFile("loi1.xml", law);
     }
 
     private void Deserialize()
