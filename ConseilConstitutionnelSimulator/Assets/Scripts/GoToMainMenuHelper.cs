5181305ae7cba28958dd72f75f4e58cdc97f4bda
Tiras69 <mathm69@gmail.com>
2017-04-09 09:59:08 +0200

    musics integrated + better transitions

---
+++ ConseilConstitutionnelSimulator/Assets/Scripts/GoToMainMenuHelper.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoToMainMenuHelper : MonoBehaviour {
+
+	public void GoToMainMenu()
+    {
+        LevelManager.Instance.LoadLevel("MainMenu");
+    }
+}
--- ConseilConstitutionnelSimulator/Assets/Scripts/LevelManager.cs
+++ ConseilConstitutionnelSimulator/Assets/Scripts/LevelManager.cs
@@ -18,6 +18,38 @@ public class LevelManager : Singleton<LevelManager> {
     yield return new WaitForSeconds(3.0f);
     //Application.LoadLevel(levelString);
     SceneManager.LoadScene(levelString);
-
+    PlayMusicOnLoad(levelString); 
   }
+
+    private void PlayMusicOnLoad(string _levelString)
+    {
+        SoundController _controller = FindObjectOfType<SoundController>();
+        switch (_levelString)
+        {
+            case "MainMenu":
+                {
+                    SoundManager.Instance.PlayMusic(_controller["ecranTitre"]);
+                }
+                break;
+
+            case "MainScene":
+                {
+                    SoundManager.Instance.PlayMusic(_controller["inGame"]);
+                }
+                break;
+
+            case "LoseScene":
+                {
+                    SoundManager.Instance.PlayMusic(_controller["loseMenu"]);
+                }
+                break;
+
+            default:
+                {
+
+                }
+                break;
+        }
+    }
+
 }
